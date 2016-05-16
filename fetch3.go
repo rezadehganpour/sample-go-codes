6855f83ebbfd9094ff83dccb8d628c09f17c95ee
rezadehganpour <rezadehganpour@gmail.com>
2016-05-15 23:58:15 -0400

    Finish up the fetch 3

---
+++ fetch3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "This url has a problem: %s, This is the error: %v", url, err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "This is the error: %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+		fmt.Printf("%s", resp.Status)
+	}
+}
